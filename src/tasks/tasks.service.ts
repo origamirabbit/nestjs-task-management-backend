201d61af1fbf5cce14b040c2165f5a784f23d91d
River <river_liu@live.cn>
2022-03-16 20:16:33 +0800

    feat: add task entity

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -33,7 +33,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID \"${id}\" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto): Task {
@@ -43,7 +49,7 @@ export class TasksService {
       id: uuid(),
       title,
       description,
-      status: TaskStatus.DONE,
+      status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
@@ -52,7 +58,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
